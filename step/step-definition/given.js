06c4350bd0f1e694121a246af8214f8eeb81741f
Dheeraj Ks <81803130+dheerajks10@users.noreply.github.com>
2021-07-22 18:08:24 +0530

    Add files via upload

---
+++ step/step-definition/given.js
@@ -0,0 +1,7 @@
+const { Given} = require('@cucumber/cucumber');
+
+Given("I open url {string}",(url) => {
+    browser.url(url);
+    browser.maximizeWindow();   
+    expect(url).toEqual("/explore");
+});
