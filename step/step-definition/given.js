2ac4c25ab6e1e77a7461d8c0b64090280baa16ba
Dheeraj Ks <81803130+dheerajks10@users.noreply.github.com>
2021-07-22 18:14:55 +0530

    Delete given.js

--- step/step-definition/given.js
+++
@@ -1,7 +0,0 @@
-const { Given} = require('@cucumber/cucumber');
-
-Given("I open url {string}",(url) => {
-    browser.url(url);
-    browser.maximizeWindow();   
-    expect(url).toEqual("/explore");
-});
